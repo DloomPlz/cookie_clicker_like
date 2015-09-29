4af4f854947d96288f320973c70193cd907066bb
StanMolveau <neostan49@gmail.com>
2015-09-29 23:24:29 +0200

    Changement js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
 
 //déclaration des variables
 var compteur = 0;
-var cps = 0;
-var degats_click =1;
+
+var coeff_a_click =1;
+var coeff_x_click=1;
+var degats_click = coeff_a_click*coeff_x_click;
 var tab_ids = new Array;
-var q = new Array; //stockera les items achetés
-
-
+var q = new Array; //structure à remplacer par tablerau à 2 dimensions
+//equation math pour cps
+var coeff_a_cps = 1;
+var coeff_b_cps = 0;
+var coeff_x_cps = 0;
+var cps=coeff_a_cps*coeff_x_cps+coeff_b_cps
 //déclaration des 3 fonctions
 /*
 	pour un jeu 'infini" il faut une relation mathématique
 */
 
-function clicker(degats_click){
+function clicker(){
 
 	compteur += degats_click;
-	$("#cpt").html("<p>"+cpt+" cookies</p>");
+	$("#cpt").html("<p>"+compteur+" cookies</p>");
 	$("#cpt").append("<p>"+cps+" cookies per second</p>");
 	console.log("+1 au compteur");
 
@@ -35,7 +40,8 @@ function achat_item(id){
 		if element is in tableau
 			add =1 for this element
 		else add element to tableau with 1 quantity
-		----------------
+
+				----------------
 		| id | quantité |
 		-----------------
 		|  0 |  5       |
@@ -45,21 +51,27 @@ function achat_item(id){
 
 	*/
 
-	if $.inArray(id,tab_ids) != -1 {
+	if($.inArray(id,tab_ids) != -1 ){
 		q[ $.inArray(id,tab_ids) ] += 1;
 	} 
 	else {
 		tab_ids.push(id);
 		q.push(1);
 	}
-	historique.push(id); //reste à définir comment orchestrer le tableau
+	/*
+	nos items vont de 1 à n
+		il faut créer des plages d'items avec des actions définis : soit agissant sur le cps / soit sur les degats du click
+		exemples :
+			de 1 à 10 : vaut 1/10 de l'id et uniquement pour click
+			de 10 à 20 : vaut 1/100 de l'id pouir click -> item10 donnera un cps de 1.1
+	*/
 	cps += 0.1;
 	setInterval(function(){ update(); }, 1000);
 	
 }
 
 
-$("#cookie").on ("click",function(){
+$("#cookie").on("click",function(){
 		compteur+=degats_click;
 	$("#cpt").html("<p>"+compteur+" cookies</p>");
 	$("#cpt").append("<p>"+cps+" cookies per second</p>");
@@ -68,28 +80,17 @@ $("#cookie").on ("click",function(){
 
 function update() {
 	compteur += cps;
-	compteur = Math.round(cpt);
+	compteur = Math.round(compteur);
 	
 	$("#cpt").html("<p>"+compteur+" cookies</p>");
 	$("#cpt").append("<p>"+cps+" cookies per second</p>");
+	setInterval(function(){ update(); }, 1000);
 }
-
-$("#achat").on("click",function(){
-	if(cpt >= cps*cps)
-	{
-		cpt -= cps*cps;
-		cps += 0.1;
-		setInterval(function(){ update(); }, 1000);
-	}
-
-});
-
-
-
 
 $(".item").click(function(){
 	var itemId = jQuery(this).attr("id");
 	achat_item(itemId);
+	console.log(itemId);
 
 	/*
 	récupérer id de l'item et faire un switch case en fonction de l'id
