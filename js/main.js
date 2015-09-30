71085fba12245ca4a375037e5ae5388ed23a1b13
StanMolveau <neostan49@gmail.com>
2015-09-30 17:18:16 +0200

    Fin

--- js/main.js
+++ js/main.js
@@ -1,46 +1,51 @@
 
 //déclaration des variables
 var jeu = {
- john_cena: 0,
- click: 1,
- value: 0,
+ john_cena: 0, /* VA CONTENIR LA MONNAIE */
+ click: 1,		/* LE COEF CLIQUE SUR JOHN CENA*/
+ value: 0,		/* Le NOMBRE DE FOIS QU'ON A CLIQUE SUR JOHN*/
+ cps:0,
  item1: 1,
- item1_prix: 2000,
+ item1_prix: 20,
  item2: 1,
- item2_prix: 10000,
+ item2_prix: 100,
  item3: 0,
- item3_prix: 50000,
- bool_save:1
+ item3_prix: 2000,
+ bool_save:1,
  item4: 0,
- item4_prix: 100000,
+ item4_prix: 10000,
  item5: 0,
- item5_prix: 5000000,
+ item5_prix: 50000,
 };
-var compteur = 0;
-var degats_click = 1;
 var tab_ids = new Array;
 var q = new Array; //structure à remplacer par tablerau à 2 dimensions
-//equation math pour cps
-var cps=0;
+//equation math pour jeu.cps
 setInterval(function(){ update(); }, 1000);
 //déclaration des 3 fonctions
 /*
 	pour un jeu 'infini" il faut une relation mathématique
 */
+$("#johncena").on("click",function(){ 
+        jeu.value += jeu.click;
+        jeu.john_cena += jeu.click
+
+		$("#ajout").text("+ "+ jeu.click+"!");
+		$( "#ajout" ).show( "fast", function(){});
+		$( "#ajout" ).hide( "fast", function(){});
+		//print_cookies();
+		console.log("+degats_click au value");
+		$("#nb_click").text("Tu as cliqué john cena " + jeu.value + " fois !");
+		$("#johncena_cpt").text("Tu as " + jeu.john_cena + " JOHN CENA à ta dispo ! ");
+});
 
 function clicker(){
-	compteur += degats_click;
-	console.log("au compteur :", degats_click);
+	jeu.value += jeu.click;
+	console.log("au value :", jeu.click);
 }
 function achat_item(id){
-	//checker si cookies suffisant pour acheter
-	var price = id * 10;
-	if(compteur >= price){
-		//soustraire prix item aux cookies
-		compteur -= price;
-	}
-	//afficher nouvelle quantité cookies
-	print_cookies();
+	
+		
+	
 	//ajout ou update item dans inventaire
 	if($.inArray(id,tab_ids) != -1 ){
 		q[ $.inArray(id,tab_ids) ] += 1;
@@ -50,40 +55,107 @@ function achat_item(id){
 		q.push(1);
 	}
 	show_inventaire();
-	//action de l'item
+	
 	if(id==1){
-		cps += 0.5;
-		console.log("augmentation de 0.5 au coeff a du cps");
-		degats_click += degats_click;
+		if (jeu.john_cena-jeu.item1_prix>=0)
+		{
+		jeu.cps += 0.5;
+		console.log("augmentation de 0.5 au coeff a du jeu.cps");
 		console.log("on double le coeff du click");
+		jeu.john_cena -= jeu.item1_prix;
+		$("#nb_item1").append('<img id="nb_biceps" src="img/biceps.jpg" width="50" height="50"/>');
+		$("#johncena_cpt").text("Tu as " + jeu.john_cena + " JOHN CENA à ta dispo ! ");
+		jeu.item1_prix=jeu.item1_prix+jeu.item1_prix;
+		$("#item1").text('Biceps : '+ jeu.item1_prix +' JOHN CENA');
+		}
+		else
+    	{
+
+        $("#johncena_cpt").text("T'as que " + jeu.john_cena + " JOHN CENA mauvais fan.");
+    	}
 	}
+
 	if(id==2){
+		if (jeu.john_cena-jeu.item2_prix>=0)
+		{
+		console.log("augmentation des clics : +10 + valeur du clic / 5");
+		jeu.click += 10 + jeu.click/5;
+		jeu.john_cena -= jeu.item2_prix;
+		$("#nb_item2").append('<img id="nb_biceps" src="img/catcheur.jpg" width="50" height="50"/>');
+		$("#johncena_cpt").text("Tu as " + jeu.john_cena + " JOHN CENA à ta dispo ! ");
+		jeu.item2_prix=jeu.item2_prix+jeu.item2_prix+jeu.item2_prix/2;
+		$("#item2").text('Catcheur : '+ jeu.item2_prix +' JOHN CENA');
+	}
+		else
+    	{
+
+        $("#johncena_cpt").text("T'as que " + jeu.john_cena + " JOHN CENA mauvais fan.");
+    	}
 	}
 	if(id==3){
+		if (jeu.john_cena-jeu.item3_prix>=0)
+		{
+		console.log("augmentation des clics par seconde : +10 + 1/5 jeu.cps");
+		jeu.cps+= 15 + 0.2*jeu.cps;
+		jeu.john_cena -= jeu.item3_prix;
+		$("#nb_item3").append('<img id="nb_belts" src="img/belt.jpg" width="50" height="50"/>');
+		$("#johncena_cpt").text("Tu as " + jeu.john_cena + " JOHN CENA à ta dispo ! ");
+		jeu.item3_prix+=jeu.item3_prix/2;
+		$("#item3").text('CEINTURE DE CATCH : '+ jeu.item3_prix +' JOHN CENA');
+	}
+		else
+    	{
+
+        $("#johncena_cpt").text("T'as que " + jeu.john_cena + " JOHN CENA mauvais fan.");
+    	}
 	}
 	if(id==4){
+		if (jeu.john_cena-jeu.item4_prix>=0)
+		{
+		console.log("augmentation des clics : +50 + click /5");
+		jeu.click += 50 + jeu.click/5;
+		jeu.john_cena -= jeu.item4_prix;
+		jeu.item4_prix+=jeu.item4_prix/4;
+		$("#nb_item4").append('<img id="nb_ring" src="img/ring.jpg" width="50" height="50"/>');
+		$("#johncena_cpt").text("Tu as " + jeu.john_cena + " JOHN CENA à ta dispo ! ");
+		
+		$("#item4").text('Ring : '+ jeu.item4_prix +' JOHN CENA');
+	}
+	
+		else
+    	{
+
+        $("#johncena_cpt").text("T'as que " + jeu.john_cena + " JOHN CENA mauvais fan.");
+    	}
 	}
 	if(id==5){
+		if (jeu.john_cena-jeu.item5_prix>=0)
+		{
+		console.log("augmentation des clics : +50 + click /5");
+		jeu.cps += 10000 + 0.1*jeu.cps;
+		jeu.john_cena -= jeu.item5_prix;
+		$("#nb_item5").append('<img id="nb_ring" src="img/johncena1.jpg" width="50" height="50"/>');
+		$("#johncena_cpt").text("Tu as " + jeu.john_cena + " JOHN CENA à ta dispo ! ");
+		jeu.item5_prix=jeu.item5_prix*2;
+		$("#item5").text('JOHN CENA COUTE : '+ jeu.item5_prix +' JOHN CENA MINDFUCK');
+		}
+		else
+    	{
+
+        $("#johncena_cpt").text("T'as que " + jeu.john_cena + " JOHN CENA mauvais fan.");
+    	}
+
 	}
-	if(id==6){
-	}
-	if(id==7){
-	}
-	if(id==8){
-	}
-	if(id==9){
-	}
-	if(id==10){
-	}
-	//update cps ou click
+	
+	//update jeu.cps ou click
 	/*
 	nos items vont de 1 à n
-		il faut créer des plages d'items avec des actions définis : soit agissant sur le cps / soit sur les degats du click
+		il faut créer des plages d'items avec des actions définis : soit agissant sur le jeu.cps / soit sur les degats du click
 		exemples :
 			de 1 à 10 : vaut 1/10 de l'id et uniquement pour click
-			de 10 à 20 : vaut 1/100 de l'id pouir click -> item10 donnera un cps de 1.1
+			de 10 à 20 : vaut 1/100 de l'id pouir click -> item10 donnera un jeu.cps de 1.1
 	*/
-}
+};
 
 $(".item").click(function(){
 	var itemId = jQuery(this).attr("id");
@@ -98,22 +170,41 @@ $(".item").click(function(){
 		alors dps+=150 jsais pas
 	*/
 });
-$("#cookie").on("click",function(){
-		compteur+=degats_click;
-		print_cookies();
-		console.log("+degats_click au compteur");
-});
+
 
 function update() {
 	console.log("time for update");
-	compteur += cps;
-	compteur = Math.round(compteur);
+	jeu.john_cena += jeu.cps;
+	jeu.john_cena = Math.round(jeu.john_cena);
 	print_cookies();
+	if (jeu.john_cena-jeu.item3_prix>=0)
+	{
+		$( "#nb_item3" ).show( "slow", function(){});
+		$( "#nb_belts" ).show( "slow", function(){});
+		$( "#item3" ).show( "slow", function(){});
+		$( "#3" ).show( "slow", function(){});
+	}
+
+	if (jeu.john_cena-jeu.item4_prix>=0)
+	{
+		$( "#nb_item4" ).show( "slow", function(){});
+		$( "#nb_nb_ring" ).show( "slow", function(){});
+		$( "#item4" ).show( "slow", function(){});
+		$( "#4" ).show( "slow", function(){});
+	}
+
+	if (jeu.john_cena-jeu.item5_prix>=0)
+	{
+		$( "#nb_item5" ).show( "slow", function(){});
+		$( "#nb_nb_johncena1" ).show( "slow", function(){});
+		$( "#item5" ).show( "slow", function(){});
+		$( "#5" ).show( "slow", function(){});
+	}
 }
 
 function print_cookies(){
-	$("#cpt").html("<p>"+compteur+" cookies</p>");
-	$("#cpt").append("<p>"+cps+" cookies per second</p>");
+	$("#cpt").html("<p>"+jeu.john_cena+" cookies</p>");
+	$("#cpt").append("<p>"+jeu.cps+" cookies per second</p>");
 }
 function show_inventaire(){
 	var vPool="";
