4ae925e5f20e24db4aa2179426752c25ac17e3cf
Grégoire Molveau <gregoire.molveau@gmail.com>
2015-09-30 12:32:18 +0200

    working basic
    version basique fonctionnelle

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 
 //déclaration des variables
 var compteur = 0;
-
 var coeff_a_click =1;
 var coeff_x_click=1;
 var degats_click = coeff_a_click*coeff_x_click;
@@ -12,58 +11,61 @@ var coeff_a_cps = 1;
 var coeff_b_cps = 0;
 var coeff_x_cps = 0;
 var cps=coeff_a_cps*coeff_x_cps+coeff_b_cps
+setInterval(function(){ update(); }, 1000);
 //déclaration des 3 fonctions
 /*
 	pour un jeu 'infini" il faut une relation mathématique
 */
+
 function update_click(){
-	
+	degats_click = coeff_a_click*coeff_x_click;
+	console.log("ceci est les degats du click",degats_click);
 }
-
+function update_dps(){
+	cps=coeff_a_cps*coeff_x_cps+coeff_b_cps;
+	console.log("ceci est les dagats cps",cps);
+}
 function clicker(){
-
 	compteur += degats_click;
-	$("#cpt").html("<p>"+compteur+" cookies</p>");
-	$("#cpt").append("<p>"+cps+" cookies per second</p>");
-	console.log("+1 au compteur");
-
+	console.log("au compteur :", degats_click);
 }
-
-function dps(cps){
-
-
-}
-
-
-
 function achat_item(id){
-	//ins&érer relattttion mathématique ici
-	/*
-	idée algo :
-		if element is in tableau
-			add =1 for this element
-		else add element to tableau with 1 quantity
-
-				----------------
-		| id | quantité |
-		-----------------
-		|  0 |  5       |
-		-----------------
-		|  1 |  10     	|
-
-
-	*/
-
+	//checker si cookies suffisant pour acheter
+	var price = id * 10;
+	if(compteur >= price){
+		//soustraire prix item aux cookies
+		compteur -= price;
+	}
+	//afficher nouvelle quantité cookies
+	print_cookies();
+	//ajout ou update item dans inventaire
 	if($.inArray(id,tab_ids) != -1 ){
 		q[ $.inArray(id,tab_ids) ] += 1;
-	} 
+	}
 	else {
 		tab_ids.push(id);
 		q.push(1);
 	}
-	if(id>0 and if <10) {
-
+	//show_inventaire();
+	//action de l'item
+	if(id==1){
+		coeff_x_cps += 0.5;
+		console.log("augmentation de 0.5 au coeff a du cps");
+		coeff_a_click += coeff_a_click;
+		console.log("on double le coeff du click");
 	}
+	if(id==2){
+		coeff_x_cps+=coeff_x_cps;
+	}
+	if(id==3){
+	}
+	if(id==4){
+	}
+	if(id==5){
+	}
+	//update cps ou click
+	update_dps();
+	update_click();
 	/*
 	nos items vont de 1 à n
 		il faut créer des plages d'items avec des actions définis : soit agissant sur le cps / soit sur les degats du click
@@ -71,46 +73,40 @@ function achat_item(id){
 			de 1 à 10 : vaut 1/10 de l'id et uniquement pour click
 			de 10 à 20 : vaut 1/100 de l'id pouir click -> item10 donnera un cps de 1.1
 	*/
-	var price = id * 10
-	compteur -= price
-
-	cps += 0.1;
-	setInterval(function(){ update(); }, 1000);
-	
 }
 
 $(".item").click(function(){
 	var itemId = jQuery(this).attr("id");
 	achat_item(itemId);
+	console.log("achat de l'item");
 	console.log(itemId);
-
 	/*
 	récupérer id de l'item et faire un switch case en fonction de l'id
 	exemple si id=item1
 		alors dps+=15 au hazard
 		si id=item50
 		alors dps+=150 jsais pas
-
-
 	*/
 });
-
 $("#cookie").on("click",function(){
 		compteur+=degats_click;
-	$("#cpt").html("<p>"+compteur+" cookies</p>");
-	$("#cpt").append("<p>"+cps+" cookies per second</p>");
-	console.log("+1 au compteur");
+		print_cookies();
+		console.log("+degats_click au compteur");
 });
 
 function update() {
+	console.log("time for update");
 	compteur += cps;
 	compteur = Math.round(compteur);
-	
-	$("#cpt").html("<p>"+compteur+" cookies</p>");
-	$("#cpt").append("<p>"+cps+" cookies per second</p>");
-	setInterval(function(){ update(); }, 1000);
+	print_cookies();
 }
 
-
-
-
+function print_cookies(){
+	$("#cpt").html("<p>"+compteur+" cookies</p>");
+	$("#cpt").append("<p>"+cps+" cookies per second</p>");
+}
+function show_inventaire(){
+	//$("#inventory").html("<p>"+compteur+" cookies</p>");
+	console.log( $.toJSON(q) );
+	console.log( $.toJSON(tab_ids) );
+}
